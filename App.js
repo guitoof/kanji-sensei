234fe91a3885519906c7ee3a3fc628f555227f30
Guillaume Diallo-Mulliez <guillaumedm@bamlab.fr>
2018-01-21 19:56:53 +0100

    Display random kanji flip cards

--- App.js
+++ App.js
@@ -4,34 +4,59 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
+  Button,
   Platform,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  Dimensions
+} from "react-native";
+import FlipCard from "react-native-flip-card";
+import { cards } from "./data.json";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const { height } = Dimensions.get("window");
+const CARD_HEIGHT = 0.8 * height;
+
+const drawCard = cardsDeck => {
+  const index = Math.floor(Math.random() * (cards.length - 1) + 1);
+  return cardsDeck[index];
+};
 
 export default class App extends Component<{}> {
+  state = {
+    card: drawCard(cards)
+  };
+  shuffle = () => {
+    this.setState({ card: drawCard(cards) });
+  };
   render() {
+    const { card } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={styles.body}>
+          <FlipCard
+            flipHorizontal
+            flipVertical={false}
+            perspective={1000}
+            style={styles.flipCard}
+          >
+            <View style={[styles.card, styles.face]}>
+              <Text style={styles.faceText}>{card.face}</Text>
+            </View>
+            <View style={[styles.card, styles.back]}>
+              <Text style={styles.backText}>{card.back}</Text>
+            </View>
+          </FlipCard>
+        </View>
+        <View style={styles.footer}>
+          <Button
+            style={{ fontSize: 42 }}
+            title="Next"
+            onPress={this.shuffle}
+          />
+        </View>
       </View>
     );
   }
@@ -39,19 +64,37 @@ export default class App extends Component<{}> {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  body: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingHorizontal: 20,
+    paddingVertical: 40
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  flipCard: {
+    height: CARD_HEIGHT,
+    borderWidth: 0
   },
+  card: {
+    height: CARD_HEIGHT,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8
+  },
+  face: {
+    backgroundColor: "#e9eeff"
+  },
+  back: {
+    backgroundColor: "#dadef0"
+  },
+  faceText: {
+    fontSize: 80
+  },
+  backText: {
+    fontSize: 42
+  }
 });
