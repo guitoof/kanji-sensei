72ce63c9412b44f28547c90c59bab6bda7fb7549
Guillaume Diallo-Mulliez <guillaumedm@bamlab.fr>
2018-01-23 01:20:12 +0100

    Add loads of data

--- data/index.js
+++ data/index.js
@@ -1,4 +1,35 @@
-import deck1 from "./deck1.json";
-import deck2 from "./deck2.json";
+import deck1 from "./deck.1.json";
+import deck2 from "./deck.2.json";
+import deck3 from "./deck.3.json";
+import deck4 from "./deck.4.json";
+import deck5 from "./deck.5.json";
+import deck6 from "./deck.6.json";
+import deck7 from "./deck.7.json";
+import deck8 from "./deck.8.json";
+import deck9 from "./deck.9.json";
+import deck10 from "./deck.10.json";
+import deck11 from "./deck.11.json";
+import deck12 from "./deck.12.json";
+import deck13 from "./deck.13.json";
+import deck14 from "./deck.14.json";
+import deck15 from "./deck.15.json";
+import deck16 from "./deck.16.json";
 
-export const cards = [...deck1, ...deck2];
+export const cards = [
+  ...deck1,
+  ...deck2,
+  ...deck3,
+  ...deck4,
+  ...deck5,
+  ...deck6,
+  ...deck7,
+  ...deck8,
+  ...deck9,
+  ...deck10,
+  ...deck11,
+  ...deck12,
+  ...deck13,
+  ...deck14,
+  ...deck15,
+  ...deck16
+];
